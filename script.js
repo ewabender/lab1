330e72596b5ca2111491878b11ee46419daee567
ewabender <your@email.com>
2026-03-19 15:13:32 +0000

    update

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ async function uploadFile() {
     try {
         status.innerText = "Requesting upload URL...";
 
-        const response = await fetch("https://xyy3xmx76e.execute-api.eu-west-1.amazonaws.com/dev");
+        const response = await fetch("https://xyy3xmx76e.execute-api.eu-west-1.amazonaws.com/dev/upload-url");
 
         if (!response.ok) {
             throw new Error(`Failed to get upload URL: ${response.status}`);
